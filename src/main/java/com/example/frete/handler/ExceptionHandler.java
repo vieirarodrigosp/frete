f2ade540012292f77c2b1d6b9b96028f82dc51a9
Rodrigo Vieira Silva <rodrigovieira.silva-ext@ab-Inbev.com>
2023-09-27 18:20:55 -0300

    limpando o hendler e error

--- src/main/java/com/example/frete/handler/ExceptionHandler.java
+++ src/main/java/com/example/frete/handler/ExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.Instant;
 
 @ControllerAdvice
 public class ExceptionHandler {
@@ -30,81 +29,4 @@ public class ExceptionHandler {
         , request.getRequestURI());
     return ResponseEntity.status(err.getStatus()).body(err);
   }
-//  @org.springframework.web.bind.annotation.ExceptionHandler(ConstraintViolationException.class)
-//  public ResponseEntity handle(ConstraintViolationException constraintViolationException) {
-//    Set<ConstraintViolation<?>> violations = constraintViolationException.getConstraintViolations();
-//    String errorMessage = "";
-//    if (!violations.isEmpty()) {
-//      StringBuilder builder = new StringBuilder();
-//      violations.forEach(violation -> builder.append(" " + violation.getMessage()));
-//      errorMessage = builder.toString();
-//    } else {
-//      errorMessage = "ConstraintViolationException occured.";
-//    }
-//    return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-//  }
-//  @org.springframework.web.bind.annotation.ExceptionHandler(UnexpectedTypeException.class)
-//  public ResponseEntity validadedException(UnexpectedTypeException e, HttpServletRequest request) {
-//    var teste = e.getLocalizedMessage();
-//    var teste01 = e.getMessage();
-//    var teste02 = e.getCause().getSuppressed();
-//    var teste03 = e.getCause().getStackTrace();
-//    var teste04 = e.getCause().fillInStackTrace();
-//    StandardError err = new StandardError(
-//        HttpStatus.BAD_REQUEST.value()
-//        , e.getLocalizedMessage()
-//        , e.getMessage()
-//        , request.getRequestURI());
-//    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
-//  }
-
-//  @org.springframework.web.bind.annotation.ExceptionHandler(SQLGrammarException.class)
-//  public ResponseEntity handle(SQLGrammarException sQLGrammarException, HttpServletRequest request) {
-//    String errorMessage = "";
-//    String message = sQLGrammarException.getLocalizedMessage() + " invalid argument(s). Message: "
-//        + sQLGrammarException.getSQLException();
-//    var err = new StandardError(HttpStatus.BAD_REQUEST.value(), "Argument(s) not valid"
-//        , message, request.getRequestURI());
-//    return ResponseEntity.status(err.getStatus()).body(err);
-//  }
-//  @org.springframework.web.bind.annotation.ExceptionHandler(PollException.class)
-//  public ResponseEntity PollException(PollException e, HttpServletRequest request) {
-//    StandardError err = new StandardError();
-//    err.setTimestamp(Instant.now());
-//    err.setStatus(HttpStatus.CONFLICT.value());
-//    err.setError(e.getLocalizedMessage());
-//    err.setMessage(e.getMessage());
-//    err.setPath(request.getRequestURI());
-//    return ResponseEntity.status(HttpStatus.CONFLICT).body(err);
-//  }
-//  @org.springframework.web.bind.annotation.ExceptionHandler(UserVoterException.class)
-//  public ResponseEntity UserVoterException(UserVoterException e, HttpServletRequest request) {
-//    StandardError err = new StandardError();
-//    err.setTimestamp(Instant.now());
-//    err.setStatus(HttpStatus.CONFLICT.value());
-//    err.setError(e.getLocalizedMessage());
-//    err.setMessage(e.getMessage());
-//    err.setPath(request.getRequestURI());
-//    return ResponseEntity.status(HttpStatus.CONFLICT).body(err);
-//  }
-//  @org.springframework.web.bind.annotation.ExceptionHandler(UserException.class)
-//  public ResponseEntity UserVoterException(UserException e, HttpServletRequest request) {
-//    StandardError err = new StandardError();
-//    err.setTimestamp(Instant.now());
-//    err.setStatus(HttpStatus.CONFLICT.value());
-//    err.setError(e.getLocalizedMessage());
-//    err.setMessage(e.getMessage());
-//    err.setPath(request.getRequestURI());
-//    return ResponseEntity.status(HttpStatus.CONFLICT).body(err);
-//  }
-//  @org.springframework.web.bind.annotation.ExceptionHandler(AssemblyException.class)
-//  public ResponseEntity UserVoterException(AssemblyException e, HttpServletRequest request) {
-//    StandardError err = new StandardError();
-//    err.setTimestamp(Instant.now());
-//    err.setStatus(HttpStatus.CONFLICT.value());
-//    err.setError(e.getLocalizedMessage());
-//    err.setMessage(e.getMessage());
-//    err.setPath(request.getRequestURI());
-//    return ResponseEntity.status(HttpStatus.CONFLICT).body(err);
-//  }
 }
--- src/main/java/com/example/frete/handler/StandardError.java
+++ src/main/java/com/example/frete/handler/StandardError.java
@@ -9,7 +9,9 @@ public class StandardError {
   private String message;
   private String path;
 
-  public StandardError() { this.timestamp = Instant.now(); }
+  public StandardError() {
+    this.timestamp = Instant.now();
+  }
 
   public StandardError(Integer status, String error, String message, String path) {
     this();
@@ -23,39 +25,19 @@ public class StandardError {
     return timestamp;
   }
 
-  public void setTimestamp(Instant timestamp) {
-    this.timestamp = timestamp;
-  }
-
   public Integer getStatus() {
     return status;
-  }
-
-  public void setStatus(Integer status) {
-    this.status = status;
   }
 
   public String getError() {
     return error;
   }
 
-  public void setError(String error) {
-    this.error = error;
-  }
-
   public String getMessage() {
     return message;
   }
 
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
   public String getPath() {
     return path;
-  }
-
-  public void setPath(String path) {
-    this.path = path;
   }
 }
